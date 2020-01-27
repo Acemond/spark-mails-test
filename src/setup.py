98bda54eb8fe0ac1ae53b57d521fa36dba4a60df
Adrien Brunelat <adrien.brunelat@gmail.com>
2020-01-27 17:17:50 +0100

    removed useless comments

--- src/setup.py
+++ src/setup.py
@@ -6,6 +6,6 @@ setup(
    description='Test application for sorting mails over Spark',
    author='Adrien Brunelat',
    author_email='adrien.brunelat@novencia.com',
-   packages=['bnp_mails_spark'],  #same as name
-   install_requires=['pyspark', 'scipy', 'matplotlib', ''], #external packages as dependencies
+   packages=['bnp_mails_spark'],
+   install_requires=['pyspark', 'scipy', 'matplotlib', ''],
 )
