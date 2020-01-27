d77a047cb4dee872aabea3b71b3ae3f68e1c7f46
Adrien Brunelat <adrien.brunelat@gmail.com>
2020-01-27 14:56:07 +0100

    isolated test in separate package

--- src/test/test_transformations.py
+++ src/test/test_transformations.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from pyspark.sql import SparkSession, Row
+from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, LongType
 
 from dataframe.mail_repository import MailRepository
